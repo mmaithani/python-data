9263003bba9ebcc83efd10af220076beb0389be3
mohit <mohitmaithani81@gmail.com>
2018-06-18 15:59:49 +0530

    i created area of circle python file

---
+++ area of circle using function.py
@@ -0,0 +1,7 @@
+#Q.1- Create a function to calculate the area of a circle by taking radius from user.
+r=int(input("enter radius of circle:-"))
+def area():
+    areaa=3.14*r*r
+    print(areaa)
+
+area()    
