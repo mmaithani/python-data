69a9205ccef2985c047a27b7acb6e3111e7e8ae9
Mohit Maithani <mohitmaithani81@gmail.com>
2018-06-25 15:51:29 +0530

    create one new file in api folder on diferences

---
+++ api assignment/differn.py
@@ -0,0 +1,16 @@
+# Ques.4- What is a difference between library
+#  and API . Figure it out with examples.
+# Ans.4
+       # My view is that when I speak of an API, it means only the parts that are exposed to the programmer.
+       # If I speak of a 'library' then I also mean everything that is working 
+       # "under the hood", though part of the library nevertheless.
+
+
+#library example
+import time
+#above i importing time library 
+  
+
+  #api
+
+  import tweepy 
