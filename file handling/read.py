eb660d135f00b8aa6ef466fa2bf13d73449681b9
Mohit Maithani <mohitmaithani81@gmail.com>
2018-06-27 22:21:37 +0530

    create file handling assignment

---
+++ file handling/read.py
@@ -0,0 +1,15 @@
+# Q.1-  Write a Python program to read
+      # last n lines of a file
+
+f=open("abc.txt","r")
+count=0
+a=0
+for line in f:
+    count+=1
+
+f.seek(0)
+for line in f:
+    a+=1
+    if a>=count-5:
+        print(line)
+f.close()
