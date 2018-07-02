548bcb35fc70f89227af88b7e6342a3c5245f0f7
Mohit Maithani <mohitmaithani81@gmail.com>
2018-07-02 14:00:51 +0530

    create directory on database in python

---
+++ databases in python/create_database.py
@@ -0,0 +1,12 @@
+# Q.1- Create a database. Create the following tables:
+# 1. Book
+# 2. Titles
+# 3. Publishers
+# 4. Zipcodes
+# 5. AuthorsTitles
+# 6. Authors
+
+#Ans.1-
+
+import pymysql as py
+import
